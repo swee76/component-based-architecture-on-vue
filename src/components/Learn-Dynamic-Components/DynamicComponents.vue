8c94607dbbe3d1cab686fbde56bc656eb92166da
swee76 <oshinikithma1213@gmail.com>
2022-12-12 12:45:21 +0530

    keeping dynamic components alive

--- src/components/Learn-Dynamic-Components/DynamicComponents.vue
+++ src/components/Learn-Dynamic-Components/DynamicComponents.vue
@@ -2,10 +2,14 @@
   <div>
     <button @click="activeTab = 'TabA'">Tab A</button>
     <button @click="activeTab = 'TabB'">Tab B</button>
-    <button @click="activeTab = 'TabC'">Tab AC</button>
+    <button @click="activeTab = 'TabC'">Tab C</button>
 
     <!-- For the Dynamic Component we can use component HTML tag and v-bind directive with is attribute by specifying the data attribute that is used to change the component  -->
-    <component :is="activeTab" />
+    <keep-alive>
+      <component :is="activeTab" />
+    </keep-alive>
+
+    <!-- Above keep-alive wrapper is unique to the vue.js and it is used to cached the value that entered to the corresponding component and it wouldn't disappear (Keep Dynamic Component Alive even though the component is inactive) -->
 
     <!-- This is not best approach -->
     <!-- <TabA v-if="activeTab === 'TabA'" />
--- src/components/Learn-Dynamic-Components/TabC.vue
+++ src/components/Learn-Dynamic-Components/TabC.vue
@@ -1,10 +1,18 @@
 <template>
   <div>TabC Content</div>
+  <!-- User may wants to keeping Dynamic components alive, that means when the user wants to fill a form by moving to different tabs, the entered data should be cached -->
+  <!-- Vue every time creates a new instance for tab components, therefore entered data should cached using, we can use keep-alive wrapper tag which is unique to the vue -->
+  <input type="text" v-model="name" />
 </template>
 
 <script>
 export default {
   name: "TabC",
+  data() {
+    return {
+      name: "",
+    };
+  },
 };
 </script>
 
