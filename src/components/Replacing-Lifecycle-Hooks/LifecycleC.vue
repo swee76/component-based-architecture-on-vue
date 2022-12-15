fc2af6a4dc1a709099ae00e27353a555cf9464d8
swee76 <oshinikithma1213@gmail.com>
2022-12-15 12:23:49 +0530

    Replacing Lifecycle Hooks

---
+++ src/components/Replacing-Lifecycle-Hooks/LifecycleC.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>Composition API Lifecycle Hooks</div>
+</template>
+
+<script>
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+export default {
+  name: "LifecycleC",
+  setup() {
+    onBeforeMount(() => {
+      console.log("LifecycleC onBeforeMount()");
+    });
+
+    onMounted(() => {
+      console.log("LifecycleC onMounted()");
+    });
+
+    onBeforeUpdate(() => {
+      console.log("LifecycleC onBeforeUpdate()");
+    });
+
+    onUpdated(() => {
+      console.log("LifecycleC onUpdated()");
+    });
+
+    onBeforeUnmount(() => {
+      console.log("LifecycleC onBeforeUnmount()");
+    });
+
+    onUnmounted(() => {
+      console.log("LifecycleC onUnmounted()");
+    });
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Replacing-Lifecycle-Hooks/LifecycleO.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>Options API Lifecycle Hooks</div>
+</template>
+
+<script>
+export default {
+  name: "LifecycleO",
+  beforeCreate() {
+    console.log("LifecycleO beforeCreate()");
+  },
+  created() {
+    console.log("LifecycleO created()");
+  },
+  beforeMount() {
+    console.log("LifecycleO beforeMount()");
+  },
+  mounted() {
+    console.log("LifecycleO mounted()");
+  },
+  beforeUpdate() {
+    console.log("LifecycleO beforeUpdate()");
+  },
+  updated() {
+    console.log("LifecycleO updated()");
+  },
+  beforeUnmount() {
+    console.log("LifecycleO beforeUnmount()");
+  },
+  unmounted() {
+    console.log("LifecycleO unmounted()");
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
